1c48bf54a9857dd25a609c768220af3e7d6c2232
Brendan Wood <wood@synchroverge.com>
2022-01-26 00:27:51 -0400

    refactor: added generic create_request, sign_request, and send_request methods to signature demo

--- demos/getting-started/signature.py
+++ demos/getting-started/signature.py
@@ -26,36 +26,56 @@ consumer_key_encoded = CONSUMER_KEY.encode()
 
 base_api = "https://api.passiv.com/api/v1"
 
+
+def create_request(endpoint, method, data=None, user_id=None):
+    params = dict(
+        clientId=CLIENT_ID,
+        timestamp=int(time.time()),
+    )
+    if user_id is not None:
+        params.update(dict(userId=user_id))
+
+    url = base_api + endpoint
+
+    req = requests.Request(
+        method=method,
+        url=url,
+        params=params,
+        data=data,
+    )
+
+    return sign_request(req)
+
+def sign_request(req):
+    parsed_url = urlparse(req.url)
+    request_path = parsed_url.path
+    request_query = urlencode(req.params)
+
+    sig_object = {"content": req.data if req.data != [] else None, "path": request_path, "query": request_query}
+
+    sig_content = json.dumps(sig_object, separators=(",", ":"), sort_keys=True)
+    sig_digest = hmac.new(consumer_key_encoded, sig_content.encode(), sha256).digest()
+
+    signature = b64encode(sig_digest).decode()
+
+    req.headers["Signature"] = signature
+
+    return req
+
+def send_request(req):
+    session = requests.Session()
+    prepped = session.prepare_request(req)
+    res = session.send(prepped)
+    return res
+
 holdings_endpoint = "/snapTrade/holdings"
 
-params = dict(
-    clientId=CLIENT_ID,
-    userId=USER_ID,
-    timestamp=int(time.time()),
+req = create_request(
+    holdings_endpoint,
+    "get",
+    user_id=USER_ID,
 )
 
-url = base_api + holdings_endpoint
-
-req = requests.Request(
-    method="get",
-    url=base_api + holdings_endpoint,
-    params=params,
-)
-
-request_data = None
-
-parsed_url = urlparse(url)
-request_path = parsed_url.path
-request_query = urlencode(params)
-
-sig_object = {"content": request_data, "path": request_path, "query": request_query}
-
-sig_content = json.dumps(sig_object, separators=(",", ":"), sort_keys=True)
-sig_digest = hmac.new(consumer_key_encoded, sig_content.encode(), sha256).digest()
-
-signature = b64encode(sig_digest).decode()
-
-req.headers["Signature"] = signature
+res = send_request(req)
 
 
-res = requests.get(url, headers=dict(Signature=signature), params=params, data=request_data)
