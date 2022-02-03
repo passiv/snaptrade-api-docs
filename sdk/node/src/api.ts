8d243aa7a11f80fc6cdde90b1c794ca976695a76
Shayan <shayanabedi.dev@gmail.com>
2022-02-03 15:48:53 -0500

    add account information endpoints

--- sdk/node/src/api.ts
+++ sdk/node/src/api.ts
@@ -17,7 +17,7 @@ class SnapTradeFetch {
 
   /**
    * @param userId
-   * Register user
+   * Register user with SnapTrade
    */
   async registerUser(userId: string) {
     const response = await request(
@@ -36,7 +36,7 @@ class SnapTradeFetch {
   /**
    * @param userId
    * @param userSecret
-   * Generate a redirect URI for user to login
+   * Generate a redirect URI to securely login a user to the SnapTrade Connection Portal
    */
 
   async generateRedirectURI(userId: string, userSecret: string) {
@@ -80,11 +80,77 @@ class SnapTradeFetch {
 
   /**
    * @param userId
-   * Fetch user holdings
+   * List all accounts for the user, plus balances and positions for each account
    */
   async fetchUserHoldings(userId: string) {
     const response = await request(
-      "/api/v1/snapTrade/holdings",
+      "/api/v1/holdings",
+      "get",
+      this.clientId,
+      this.consumerKey,
+      userId,
+      null
+    );
+    return response;
+  }
+
+  /**
+   * @param userId
+   * List all investment accounts for the user
+   */
+  async fetchUserAccounts(userId: string) {
+    const response = await request(
+      "/api/v1/accounts",
+      "get",
+      this.clientId,
+      this.consumerKey,
+      userId,
+      null
+    );
+    return response;
+  }
+  /**
+   * @param userId
+   * @param accountId
+   * Return details of a specific investment account
+   */
+  async fetchAccount(userId: string, accountId: string) {
+    const response = await request(
+      `/api/v1/accounts/${accountId}`,
+      "get",
+      this.clientId,
+      this.consumerKey,
+      userId,
+      null
+    );
+    return response;
+  }
+
+  /**
+   * @param userId
+   * @param accountId
+   * Get all cash balances of an investment account
+   */
+  async fetchAccountBalances(userId: string, accountId: string) {
+    const response = await request(
+      `/api/v1/accounts/${accountId}/balances`,
+      "get",
+      this.clientId,
+      this.consumerKey,
+      userId,
+      null
+    );
+    return response;
+  }
+
+  /**
+   * @param userId
+   * @param accountId
+   * Get all positions of an investment account
+   */
+  async fetchAccountPositions(userId: string, accountId: string) {
+    const response = await request(
+      `/api/v1/accounts/${accountId}/positions`,
       "get",
       this.clientId,
       this.consumerKey,
